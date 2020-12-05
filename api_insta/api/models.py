18e0e9eba650e2a781088db3ca68a3ad8a1d75dc
monda00 <monda0524@gmail.com>
2020-12-05 13:48:15 +0900

    fix typo

--- api_insta/api/models.py
+++ api_insta/api/models.py
@@ -27,7 +27,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
-        user.is_stagg = True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
