a1e4934ef614a7fbdfef5f5a896e04ab15f4556d
monda00 <monda0524@gmail.com>
2020-12-15 20:36:05 +0900

    add api method

--- front_insta/src/features/auth/authSlice.ts
+++ front_insta/src/features/auth/authSlice.ts
@@ -2,8 +2,91 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 import axios from "axios";
 import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types";
+import { profile } from 'console';
 
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
+
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post('${apiUrl}authen/jwt/create', authen, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncRegister = createAsyncThunk(
+  "auth/register",
+  async (auth: PROPS_AUTHEN) => {
+    const res = await axios.post('${apiUrl}api/register/', auth, {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncCreateProf = createAsyncThunk(
+  "profile/post",
+  async (nickName: PROPS_NICKNAME) => {
+    const res = await axios.post('${apiUrl}api/profile/', nickName, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: 'JWT ${localStorage.localJWT}',
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncUpdateProf = createAsyncThunk(
+  "profile/put",
+  async (prifile: PROPS_PROFILE) => {
+    const uploadData = new FormData();
+    uploadData.append("nickName", profile.nickName);
+    profile.img && uploadData.append("img", profile.img, profile.img.name);
+    const res = await axios.put(
+      '${apiUrl}api/profile/${profile.id}',
+      uploadData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: 'JWT ${localStorage.localJWT}',
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
+export const fetchAsyncGetMyProf = createAsyncThunk(
+  "profile/get",
+  async () => {
+    const res = await axios.get('${apiUrl}api/profile/', {
+      headers: {
+        Authorization: 'JWT ${localStorage.localJWT}',
+      },
+    });
+    return res.data[0];
+  }
+);
+
+export const fetchAsyncGetProfs = createAsyncThunk(
+  "profiles/get",
+  async () => {
+    const res = await axios.get('${apiUrl}api/profile/', {
+      headers: {
+        Authorization: 'JWT ${localStorage.localJWT}',
+      },
+    });
+    return res.data;
+  }
+);
+
 
 export const authSlice = createSlice({
   name: 'auth',
