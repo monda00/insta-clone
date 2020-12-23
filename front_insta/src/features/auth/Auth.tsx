3085c82d196fed61a7a75433c3a103936ad885f1
monda00 <monda0524@gmail.com>
2020-12-23 21:47:03 +0900

    add signup modal

--- front_insta/src/features/auth/Auth.tsx
+++ front_insta/src/features/auth/Auth.tsx
@@ -43,13 +43,121 @@ const customStyles = {
 export const Auth: React.FC = () => {
   Modal.setAppElement("#root");
   const openSignIn = useSelector(selectOpenSignIn);
-  const openSignUp = useLecelctor(selectOpenSignUp);
+  const openSignUp = useSelector(selectOpenSignUp);
   const isLoadingAuth = useSelector(selectIsLoadingAuth);
   const dispatch: AppDispatch = useDispatch();
 
   return (
-    <div>
+    <>
+      <Modal
+        isOpen={openSignUp}
+        onRequestClose={async () => {
+          await dispatch(resetOpenSignUp());
+        }}
+        style={customStyles}
+      >
 
-    </div>
+        <Formik
+          initialErrors={{ email: "required" }}
+          initialValues={{ email: "", password: "" }}
+          onSubmit={async (values) => {
+            await dispatch(fetchCredStart());
+            const resultReg = await dispatch(fetchAsyncRegister(values));
+
+            if (fetchAsyncRegister.fulfilled.match(resultReg)) {
+              await dispatch(fetchAsyncLogin(values));
+              await dispatch(fetchAsyncCreateProf({ nickName: "anonymous" }));
+
+              await dispatch(fetchAsyncGetProfs());
+              //await dispatch(fetchAsyncGetPosts());
+              //await dispatch(fetchAsyncGetComments());
+              await dispatch(fetchAsyncGetMyProf());
+            }
+            await dispatch(fetchCredEnd());
+            await dispatch(resetOpenSignUp());
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email("email format is wrong")
+              .required("email is must"),
+            password: Yup.string().required("password is must").min(4),
+          })}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            touched,
+            isValid,
+          }) => (
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div className={styles.auth_signUp}>
+                  <h1 className={styles.auth_title}>SNS clone</h1>
+                  <br />
+                  <div className={styles.auth_progress}>
+                    {isLoadingAuth && <CircularProgress />}
+                  </div>
+                  <br />
+
+                  <TextField
+                    placeholder="email"
+                    type="input"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  <br />
+                  {touched.email && errors.email ? (
+                    <div className={styles.auth_error}>{errors.email}</div>
+                  ) : null}
+
+                  <TextField
+                    placeholder="password"
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                  {touched.password && errors.password ? (
+                    <div className={styles.auth_error}>{errors.password}</div>
+                  ) : null}
+                  <br />
+                  <br />
+
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    disabled={!isValid}
+                    type="submit"
+                  >
+                    Register
+                  </Button>
+                  <br />
+                  <br />
+
+                  <span
+                    className={styles.auth_text}
+                    onClick={async () => {
+                      await dispatch(setOpenSignIn());
+                      await dispatch(resetOpendSignUp());
+                    }}
+                  >
+                    You already have a account ?
+                  </span>
+
+                </div>
+              </form>
+            </div>
+          )}
+
+        </Formik>
+
+      </Modal>
+    </>
   )
 }
