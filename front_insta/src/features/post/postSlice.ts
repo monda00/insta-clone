5452360b889dc4f20300656102f0a767f2f2b727
monda00 <monda0524@gmail.com>
2021-01-09 18:07:00 +0900

    add postSlice

---
+++ front_insta/src/features/post/postSlice.ts
@@ -0,0 +1,175 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
+import { PROPS_NEWPOST, PROPS_LIKED, PROPS_COMMENT } from "../types";
+import { create } from "yup/lib/Reference";
+
+const apiUrlPost = `${process.env.REACT_APP_DEV_API_URL}api/post/`;
+const apiUrlComment = `${process.env.REACT_APP_DEV_API_URL}api/comment/`;
+
+export const fetchAsyncGetPosts = createAsyncThunk("post/get", async () => {
+  const res = await axios.get(apiUrlPost, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
+export const fetchAsyncNewPost = createAsyncThunk(
+  "post/post",
+  async (newPost: PROPS_NEWPOST) => {
+    const uploadData = new FormData();
+    uploadData.append("title", newPost.title);
+    newPost.img && uploadData.append("img", newPost.img, newPost.img.name);
+    const res = await axios.post(apiUrlPost, uploadData, {
+      headers: {
+        "Context-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncPatchLiked = createAsyncThunk(
+  "post/patch",
+  async (liked: PROPS_LIKED) => {
+    const currentLiked = liked.current;
+    const uploadData = new FormData();
+
+    let isOverlapped = false;
+    currentLiked.forEach((current) => {
+      if (current === liked.new) {
+        isOverlapped = true;
+      } else {
+        uploadData.append("liked", String(current));
+      }
+    });
+
+    if (!isOverlapped) {
+      uploadData.append("liked", String(liked.new));
+    } else if (currentLiked.length === 1) {
+      uploadData.append("title", liked.title);
+      const res = await axios.put(`${apiUrlPost}${liked.id}/`, uploadData, {
+        headers: {
+          "Context-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      });
+      return res.data;
+    }
+    const res = await axios.patch(`${apiUrlPost}${liked.id}/`, uploadData, {
+      headers: {
+        "Context-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const fetchAsyncGetComments = createAsyncThunk(
+  "comment/get",
+  async () => {
+    const res = await axios.get(apiUrlComment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    })
+    return res.data;
+  }
+);
+
+export const fetchAsyncPostComment = createAsyncThunk(
+  "comment/post",
+  async (comment: PROPS_COMMENT) => {
+    const res = await axios.post(apiUrlComment, comment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+export const postSlice = createSlice({
+  name: "post",
+  initialState: {
+    isLoadingPost: false,
+    openNewPost: false,
+    posts: [
+      {
+        id: 0,
+        title: "",
+        userPost: 0,
+        created_on: "",
+        img: "",
+        liked: [0],
+      },
+    ],
+    comments: [
+      {
+        id: 0,
+        text: "",
+        userComment: 0,
+        post: 0,
+      },
+    ],
+  },
+  reducers: {
+    fetchPostStart(state) {
+      state.isLoadingPost = true;
+    },
+    fetchPostEnd(state) {
+      state.isLoadingPost = false;
+    },
+    setOpenNewPost(state) {
+      state.openNewPost = true;
+    },
+    resetOpenNewPost(state) {
+      state.openNewPost = false;
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncLogin.fulfilled, (state, action) => {
+      localStorage.setItem("localJWT", action.payload.access);
+    });
+    builder.addCase(fetchAsyncCreateProf.fulfilled, (state, action) => {
+      state.myprofile = action.payload;
+    });
+    builder.addCase(fetchAsyncGetMyProf.fulfilled, (state, action) => {
+      state.myprofile = action.payload;
+    });
+    builder.addCase(fetchAsyncGetProfs.fulfilled, (state, action) => {
+      state.profiles = action.payload;
+    });
+    builder.addCase(fetchAsyncUpdateProf.fulfilled, (state, action) => {
+      state.myprofile = action.payload;
+      state.profiles = state.profiles.map((prof) =>
+        prof.id === action.payload.id ? action.payload : prof
+      );
+    });
+  },
+});
+
+export const {
+  fetchCredStart,
+  fetchCredEnd,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  setOpenProfile,
+  resetOpenProfile,
+  editNickname,
+} = authSlice.actions;
+
+export const selectIsLoadingAuth = (state: RootState) => state.auth.isLoadingAuth;
+export const selectOpenSignIn = (state: RootState) => state.auth.openSignIn;
+export const selectOpenSignUp = (state: RootState) => state.auth.openSignUp;
+export const selectOpenProfile = (state: RootState) => state.auth.openProfile;
+export const selectProfile = (state: RootState) => state.auth.myprofile;
+export const selectProfiles = (state: RootState) => state.auth.profiles;
+
+export default authSlice.reducer;
