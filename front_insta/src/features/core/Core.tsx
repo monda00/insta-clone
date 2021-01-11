6e02254ed2887b3464dd0862b791ae5da863db43
monda00 <monda0524@gmail.com>
2021-01-11 11:02:27 +0900

    update core component

--- front_insta/src/features/core/Core.tsx
+++ front_insta/src/features/core/Core.tsx
@@ -109,6 +109,7 @@ const Core: React.FC = () => {
               <MdAddAPhoto />
             </button>
             <div className={styles.core_logout}>
+              {(isLoadingPost || isLoadingAuth) && <CircularProgress />}
               <Button
                 onClick={() => {
                   localStorage.removeItem("localJWT");
@@ -120,11 +121,72 @@ const Core: React.FC = () => {
               >
                 Logout
               </Button>
+
+              <button
+                className={styles.core_btnModal}
+                onClick={() => {
+                  dispatch(setOpenProfile());
+                  dispatch(resetOpenNewPost());
+                }}
+              >
+                <StyledBadge
+                  overlap="circle"
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  variant="dot"
+                >
+                  <Avatar alt="who?" src={profile.img} />
+                </StyledBadge>
+              </button>
             </div>
-          </>) :
-            <div></div>
-        }
+          </>
+        ) : (
+          <div>
+            <Button
+                onClick={() => {
+                  dispatch(setOpenSignIn());
+                  dispatch(resetOpenSignUp());
+              }}
+            >
+              LogIn
+            </Button>
+            <Button
+                onClick={() => {
+                  dispatch(setOpenSignUp());
+                  dispatch(resetOpenSignIn());
+              }}
+            >
+              SignUp
+            </Button>
+          </div>
+        )}
       </div>
+
+      {profile.nickName && (
+        <>
+          <div className={styles.core_posts}>
+            <Grid container spacing={4}>
+              {posts
+                .slice(0)
+                .reverse()
+                .map((post) => (
+                  <Grid key={post.id} item xs={12} md={4}>
+                    <Post
+                      postId={post.id}
+                      title={post.title}
+                      loginId={profile.userProfile}
+                      userPost={post.userPost}
+                      imageUrl={post.img}
+                      liked={post.liked}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
+          </div>
+        </>
+      )}
     </div>
   )
 }
