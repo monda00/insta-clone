7a93c5bfde1465558bdab1596240b21bdc555915
monda00 <monda0524@gmail.com>
2021-01-11 10:46:03 +0900

    add menu bar

--- front_insta/src/features/core/Core.tsx
+++ front_insta/src/features/core/Core.tsx
@@ -95,6 +95,36 @@ const Core: React.FC = () => {
   return (
     <div>
       <Auth />
+      <div className={styles.core_header}>
+        <h1 className={styles.core_title}>SNS clone</h1>
+        {profile?.nickName ? (
+          <>
+            <button
+              className={styles.core_btnModal}
+              onClick={() => {
+                dispatch(setOpenNewPost());
+                dispatch(resetOpenProfile());
+              }}
+            >
+              <MdAddAPhoto />
+            </button>
+            <div className={styles.core_logout}>
+              <Button
+                onClick={() => {
+                  localStorage.removeItem("localJWT");
+                  dispatch(editNickname(""));
+                  dispatch(resetOpenProfile());
+                  dispatch(resetOpenNewPost());
+                  dispatch(setOpenSignIn());
+                }}
+              >
+                Logout
+              </Button>
+            </div>
+          </>) :
+            <div></div>
+        }
+      </div>
     </div>
   )
 }
