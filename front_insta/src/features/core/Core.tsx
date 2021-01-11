845d7db706d9bf878c74a79c9428662f7181fe9b
monda00 <monda0524@gmail.com>
2021-01-11 11:45:44 +0900

    add post component

--- front_insta/src/features/core/Core.tsx
+++ front_insta/src/features/core/Core.tsx
@@ -39,6 +39,8 @@ import {
   fetchAsyncGetComments,
 } from "../post/postSlice";
 
+import Post from "../post/Post";
+
 const StyledBadge = withStyles((theme) => ({
   badge: {
     backgroundColor: '#44b700',
